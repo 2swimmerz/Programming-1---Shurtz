efc6d714d2539e43d4c8827e1404d6cf53d11e51
2swimmerz <shsamhenderson@gmail.com>
2015-03-19 10:15:53 -0600

    Created IYOGWP - 4a.py

---
+++ IYOGWP - 4a.py
@@ -0,0 +1,31 @@
+# This is a guess the number game.
+import random
+
+guessesTaken = 0
+
+number = random.randint(10, 30)
+print('Well, I am thinking of a number between 10 and 30.')
+
+while guessesTaken < 5:
+    print('Take a guess.') # There are four spaces in front of print.
+    guess = input()
+    guess = int(guess)
+
+    guessesTaken = guessesTaken + 1
+
+    if guess < number:
+        print('Your guess is too low.') # There are eight spaces in front of print.
+
+    if guess > number:
+        print('Your guess is too high.')
+
+    if guess == number:
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken)
+    print('Good job! You guessed my number in ' + guessesTaken + ' guesses!')
+
+if guess != number:
+    number = str(number)
+    print('Nope. The number I was thinking of was ' + number)
